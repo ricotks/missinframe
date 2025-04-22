c9fcc96d8c410bdbab4adf9454b94a3d6e90074c
Rico <rico@MacBook-Pro-de-Rico.local>
2025-04-22 12:17:47 -0300

    favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
+import remark from '@astrojs/markdown-remark';
 
-export default defineConfig({});
+export default defineConfig({
+  integrations: [remark()],
+});
